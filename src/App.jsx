40391f2d80747e63caba42fdffcc46b45b363405
Tom Griffin <t.a.griffin@gmail.com>
2020-11-15 19:38:29 +1300

    Completed App.jsx it successfully displays a picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
-//import request from 'superagent'
+import request from 'superagent'
 
 class App extends React.Component {
-  // state = {
-  //   url: '',
-  //   error: null
-  // }
+  state = {
+    url: '',
+    error: null
+  }
 
-  // componentDidMount () {
-
-  // }
+  componentDidMount () {
+    const apiURL = 'https://randomfox.ca/floof/'
+    return request
+      .get(apiURL)
+      .then(res => this.setState({ url: res.body.image }))
+      .catch(err => this.setState({ error: err }))
+  }
 
   render () {
+    const { url } = this.state
+
     return (
       <>
         <h1>Thanks randomfox.ca for the sly fox pic!</h1>
+        <img src={ url } alt="A random sly fox pic."/>
       </>
     )
   }
