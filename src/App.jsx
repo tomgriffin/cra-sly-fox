5598a79e9c3367574dc3d5ea1c1b824a4f22f419
Tom Griffin <t.a.griffin@gmail.com>
2020-11-15 18:06:42 +1300

    ------- lost a bunch of work, start over. Added App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+//import request from 'superagent'
+
+class App extends React.Component {
+  // state = {
+  //   url: '',
+  //   error: null
+  // }
+
+  // componentDidMount () {
+
+  // }
+
+  render () {
+    return (
+      <>
+        <h1>Thanks randomfox.ca for the sly fox pic!</h1>
+      </>
+    )
+  }
+}
+
+export default App
